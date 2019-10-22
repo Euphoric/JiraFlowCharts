ff2d5bfcc727975601984216ec09c68b4c4fa7a0
Radek Falhar <Radek.Falhar@hotmail.com>
2019-10-22 11:35:31 +0200

    Refactored loading of tasks into separate class.

--- Jira.FlowCharts/MainViewModel.cs
+++ Jira.FlowCharts/MainViewModel.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Caliburn.Micro;
-using Jira.Querying;
-using Jira.Querying.Sqlite;
 
 namespace Jira.FlowCharts
 {
@@ -13,82 +8,20 @@ namespace Jira.FlowCharts
     {
         private async Task InitializeAsync()
         {
-            var issues = await RetrieveIssues();
+            TasksSource source = new TasksSource();
 
-            var stories = issues
-                .Where(x => x.Type == "Story" || x.Type == "Bug")
-                .Where(x => x.Resolution != "Cancelled" && x.Resolution != "Duplicate")
-                .Where(x => x.Status != "Withdrawn" && x.Status != "On Hold");
+            FlowIssue[] finishedStories = await source.GetFinishedTasks();
 
-            var states = new[] { "Ready For Dev", "In Dev", "Ready for Peer Review", "Ready for QA", "In QA", "Ready for Done", "Done" };
-            var resetStates = new[] { "On Hold", "Not Started", "Withdrawn" };
-            SimplifyStateChangeOrder simplify = new SimplifyStateChangeOrder(states, resetStates);
-
-            DateTime startDate = DateTime.Now.AddMonths(-12);
-
-            var finishedStories = stories
-                .Where(x => x.Status == "Done")
-                .Select(x => CalculateDuration(x, simplify))
-                .Where(x => x.End > startDate).ToArray();
-
-            Items.Add(new CumulativeFlowViewModel(stories, states));
+            Items.Add(new CumulativeFlowViewModel(await source.GetAllTasks(), source.States));
             Items.Add(new CycleTimeScatterplotViewModel(finishedStories));
             Items.Add(new CycleTimeHistogramViewModel(finishedStories));
             Items.Add(new StoryPointCycleTimeViewModel(finishedStories));
             Items.Add(new SimulationViewModel(finishedStories));
-
-            //List<double> daysItTakesToFinish = new List<double>();
-            //for (int i = -200; i < 0; i++)
-            //{
-            //    var sinceTime = DateTime.Now.AddDays(i);
-
-            //    var storiesFinishedSince = finishedStories.Where(x => x.Start > sinceTime).OrderBy(x => x.End).ToArray();
-            //    if (storiesFinishedSince.Length < 10)
-            //        break;
-            //    var lastFinishedStory = storiesFinishedSince.Take(10).Last();
-
-            //    var timeItTakesToFinishStories = (lastFinishedStory.End - sinceTime).TotalDays;
-            //    daysItTakesToFinish.Add(timeItTakesToFinishStories);
-            //}
         }
 
         protected override async Task OnInitializeAsync(CancellationToken cancellationToken)
         {
             await InitializeAsync();
-        }
-
-        private static FlowIssue CalculateDuration(CachedIssue issue, SimplifyStateChangeOrder simplify)
-        {
-            var simplifiedIssues = simplify.FilterStatusChanges(issue.StatusChanges);
-
-            var startTime = simplifiedIssues.First().ChangeTime;
-            var doneTime = simplifiedIssues.Last().ChangeTime;
-
-            TimeSpan duration = doneTime - startTime;
-
-            var flowIssue = new FlowIssue()
-            {
-                Key = issue.Key,
-                Title = issue.Title,
-                Type = issue.Type,
-                Start = startTime,
-                End = doneTime,
-                Duration = duration.TotalDays,
-                StoryPoints = issue.StoryPoints,
-                TimeSpent = issue.TimeSpent
-            };
-
-            return flowIssue;
-        }
-
-        private static async Task<List<CachedIssue>> RetrieveIssues()
-        {
-            using (var cacheRepo = new SqliteJiraLocalCacheRepository(@"../../../Data/issuesCache.db"))
-            {
-                await cacheRepo.Initialize();
-
-                return (await cacheRepo.GetIssues()).ToList();
-            }
         }
     }
 }
---
+++ Jira.FlowCharts/TasksSource.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jira.Querying;
+using Jira.Querying.Sqlite;
+
+namespace Jira.FlowCharts
+{
+    public class TasksSource
+    {
+        public string[] States { get; }
+        public string[] ResetStates { get; }
+
+        public TasksSource()
+        {
+            States = new[] { "Ready For Dev", "In Dev", "Ready for Peer Review", "Ready for QA", "In QA", "Ready for Done", "Done" };
+            ResetStates = new[] { "On Hold", "Not Started", "Withdrawn" };
+        }
+
+        private static async Task<List<CachedIssue>> RetrieveIssues()
+        {
+            using (var cacheRepo = new SqliteJiraLocalCacheRepository(@"../../../Data/issuesCache.db"))
+            {
+                await cacheRepo.Initialize();
+
+                return (await cacheRepo.GetIssues()).ToList();
+            }
+        }
+
+        public async Task<IEnumerable<CachedIssue>> GetAllTasks()
+        {
+            var issues = await RetrieveIssues();
+
+            IEnumerable<CachedIssue> stories = issues
+                .Where(x => x.Type == "Story" || x.Type == "Bug")
+                .Where(x => x.Resolution != "Cancelled" && x.Resolution != "Duplicate")
+                .Where(x => x.Status != "Withdrawn" && x.Status != "On Hold");
+
+            return stories;
+        }
+
+        public async Task<FlowIssue[]> GetFinishedTasks()
+        {
+            IEnumerable<CachedIssue> stories = await GetAllTasks();
+
+            SimplifyStateChangeOrder simplify = new SimplifyStateChangeOrder(States, ResetStates);
+
+            DateTime startDate = DateTime.Now.AddMonths(-12);
+
+            FlowIssue[] finishedStories = stories
+                .Where(x => x.Status == "Done")
+                .Select(x => CalculateDuration(x, simplify))
+                .Where(x => x.End > startDate).ToArray();
+
+            return finishedStories;
+        }
+
+        private static FlowIssue CalculateDuration(CachedIssue issue, SimplifyStateChangeOrder simplify)
+        {
+            var simplifiedIssues = simplify.FilterStatusChanges(issue.StatusChanges);
+
+            var startTime = simplifiedIssues.First().ChangeTime;
+            var doneTime = simplifiedIssues.Last().ChangeTime;
+
+            TimeSpan duration = doneTime - startTime;
+
+            var flowIssue = new FlowIssue()
+            {
+                Key = issue.Key,
+                Title = issue.Title,
+                Type = issue.Type,
+                Start = startTime,
+                End = doneTime,
+                Duration = duration.TotalDays,
+                StoryPoints = issue.StoryPoints,
+                TimeSpent = issue.TimeSpent
+            };
+
+            return flowIssue;
+        }
+    }
+}
