9df08d3d7608a36e7cfe065e70d904b640d311ff
Radek Falhar <Radek.Falhar@hotmail.com>
2019-10-22 11:02:34 +0200

    StoryPointSycleTime updated to use OnActivate.

--- Jira.FlowCharts/StoryPointCycleTimeViewModel.cs
+++ Jira.FlowCharts/StoryPointCycleTimeViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using Caliburn.Micro;
@@ -13,13 +15,33 @@ namespace Jira.FlowCharts
 {
     public class StoryPointCycleTimeViewModel : Screen
     {
+        private readonly FlowIssue[] _flowIssues;
+        private string[] _labels;
+        private SeriesCollection _seriesCollection;
+
+        public SeriesCollection SeriesCollection
+        {
+            get => _seriesCollection;
+            private set => Set(ref _seriesCollection, value);
+        }
+
+        public string[] Labels
+        {
+            get => _labels;
+            set => Set(ref _labels, value);
+        }
+
         public StoryPointCycleTimeViewModel(FlowIssue[] flowIssues)
         {
+            _flowIssues = flowIssues;
             DisplayName = "Story point vs. cycle time";
+        }
 
-            var storyPointGrouped = flowIssues
+        protected override Task OnActivateAsync(CancellationToken cancellationToken)
+        {
+            var storyPointGrouped = _flowIssues
                 .Where(x => x.StoryPoints.HasValue)
-                .Where(x=>x.StoryPoints.Value > 0)
+                .Where(x => x.StoryPoints.Value > 0)
                 .GroupBy(x => x.StoryPoints.Value)
                 .Select(grp => new
                 {
@@ -30,11 +52,11 @@ namespace Jira.FlowCharts
                     Percentile75 = Percentile(grp.Select(x => x.Duration), 0.75),
                     Percentile95 = Percentile(grp.Select(x => x.Duration), 0.95),
                     Max = grp.Max(x => x.Duration),
-                    Average = grp.Average(x=>x.Duration),
+                    Average = grp.Average(x => x.Duration),
                     Median = Percentile(grp.Select(x => x.Duration), 0.5),
                     Count = grp.Count()
                 })
-                .OrderBy(x=>x.StoryPoints)
+                .OrderBy(x => x.StoryPoints)
                 .ToArray();
 
 
@@ -65,32 +87,14 @@ namespace Jira.FlowCharts
             };
 
             Labels = storyPointGrouped.Select(x => x.StoryPoints.ToString()).ToArray();
+
+            return base.OnActivateAsync(cancellationToken);
         }
 
         private double Percentile(IEnumerable<double> values, double percentile)
         {
             var ordered = values.OrderBy(x => x).ToArray();
-            return ordered[(int) (ordered.Length * percentile)];
-        }
-
-        public SeriesCollection SeriesCollection { get; set; }
-
-        private string[] _labels;
-        public string[] Labels
-        {
-            get { return _labels; }
-            set
-            {
-                _labels = value;
-                OnPropertyChanged("Labels");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName = null)
-        {
-            if (PropertyChanged != null) PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return ordered[(int)(ordered.Length * percentile)];
         }
     }
 }
