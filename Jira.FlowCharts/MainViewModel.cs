85b53416413f38144719d4d5d1a25da0ac0cd77d
Radek Falhar <Radek.Falhar@hotmail.com>
2019-10-31 15:23:07 +0100

    Added story filtering tab.

--- Jira.FlowCharts/MainViewModel.cs
+++ Jira.FlowCharts/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using Jira.FlowCharts.JiraUpdate;
+using Jira.FlowCharts.StoryFiltering;
 using Jira.Querying;
 using Jira.Querying.Sqlite;
 
@@ -11,9 +12,12 @@ namespace Jira.FlowCharts
     {
         public MainViewModel()
         {
+            DisplayName = "Jira flow metrics";
+
             TasksSource source = new TasksSource(new TasksSourceJiraCacheAdapter());
             
             Items.Add(new JiraUpdateViewModel(source, new CurrentTime()));
+            Items.Add(new StoryFilteringViewModel(source));
             Items.Add(new CumulativeFlowViewModel(source));
             Items.Add(new CycleTimeScatterplotViewModel(source));
             Items.Add(new CycleTimeHistogramViewModel(source));
---
+++ Jira.FlowCharts/StoryFiltering/StoryFilteringViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.FlowCharts.StoryFiltering
+{
+    public class StoryFilteringViewModel : ReactiveScreen
+    {
+        public StoryFilteringViewModel(TasksSource source)
+        {
+            DisplayName = "Story and state filtering";
+        }
+    }
+}
