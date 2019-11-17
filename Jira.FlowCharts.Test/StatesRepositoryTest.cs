4f4171a93ffee792903faa49257b6151d07b718e
Radek Falhar <Radek.Falhar@hotmail.com>
2019-11-17 10:47:33 +0100

    Implemented JsonStatesRepository.

---
+++ Jira.FlowCharts.Test/StatesRepositoryTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Jira.FlowCharts
+{
+    public class JsonStatesRepositoryTest : StatesRepositoryTest, IDisposable
+    {
+        private readonly string _fileName;
+
+        public JsonStatesRepositoryTest()
+        {
+            _fileName = Path.GetRandomFileName();
+        }
+
+        public void Dispose()
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+        }
+
+        protected override IStatesRepository CreateRepository()
+        {
+            return new JsonStatesRepository(_fileName);
+        }
+    }
+
+    public class MemoryStatesRepositoryTest : StatesRepositoryTest
+    {
+        MemoryStatesRepository _repository;
+
+        public MemoryStatesRepositoryTest()
+        {
+            _repository = new MemoryStatesRepository(new string[0], new string[0]);
+        }
+
+        protected override IStatesRepository CreateRepository()
+        {
+            return _repository;
+        }
+    }
+
+    public abstract class StatesRepositoryTest
+    {
+        protected abstract IStatesRepository CreateRepository();
+
+        IStatesRepository Repository => CreateRepository();
+
+        [Fact]
+        public void Is_empty_after_creation()
+        {
+            Assert.Empty(Repository.GetFilteredStates());
+            Assert.Empty(Repository.GetResetStates());
+        }
+
+        [Fact]
+        public void Setting_filtered_state_stays_can_be_retrieved()
+        {
+            string[] states = new string[] { "A", "B", "C" };
+            
+            Repository.SetFilteredStates(states);
+
+            Assert.Equal(states, Repository.GetFilteredStates());
+            Assert.Empty(Repository.GetResetStates());
+        }
+
+        [Fact]
+        public void Setting_reset_state_stays_can_be_retrieved()
+        {
+            string[] states = new string[] { "A", "B", "C" };
+
+            Repository.SetResetStates(states);
+
+            Assert.Equal(states, Repository.GetResetStates());
+            Assert.Empty(Repository.GetFilteredStates());
+        }
+
+        [Fact]
+        public void Setting_all_states_can_be_retrieved()
+        {
+            string[] filteredStates = new string[] { "E", "F", "G" };
+            string[] resetStates = new string[] { "A", "B", "C" };
+
+            Repository.SetFilteredStates(filteredStates);
+            Repository.SetResetStates(resetStates);
+
+            Assert.Equal(filteredStates, Repository.GetFilteredStates());
+            Assert.Equal(resetStates, Repository.GetResetStates());
+
+            Repository.SetResetStates(resetStates);
+            Repository.SetFilteredStates(filteredStates);
+
+            Assert.Equal(filteredStates, Repository.GetFilteredStates());
+            Assert.Equal(resetStates, Repository.GetResetStates());
+        }
+    }
+}
---
+++ Jira.FlowCharts/JsonStatesRepository.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Jira.FlowCharts
+{
+    public class JsonStatesRepository : IStatesRepository
+    {
+        private class JsonStructure
+        {
+            public string[] FilteredStates { get; set; }
+            public string[] ResetStates { get; set; }
+        }
+
+        private readonly string _file;
+
+        public JsonStatesRepository(string file)
+        {
+            _file = file;
+        }
+
+        private JsonStructure CurrentStructure()
+        {
+            JsonStructure jsonStructure;
+            if (!File.Exists(_file))
+            {
+                jsonStructure = new JsonStructure()
+                {
+                    FilteredStates = new string[0],
+                    ResetStates = new string[0]
+                };
+            }
+            else
+            {
+                jsonStructure = JsonConvert.DeserializeObject<JsonStructure>(File.ReadAllText(_file));
+            }
+
+            return jsonStructure;
+        }
+
+        private void SaveNewStructure(JsonStructure structure)
+        {
+            File.WriteAllText(_file, JsonConvert.SerializeObject(structure));
+        }
+
+        public string[] GetFilteredStates()
+        {
+            JsonStructure jsonStructure = CurrentStructure();
+            return jsonStructure.FilteredStates;
+        }
+
+        public void SetFilteredStates(string[] states)
+        {
+            var structure = CurrentStructure();
+            structure.FilteredStates = states;
+            SaveNewStructure(structure);
+        }
+
+        public string[] GetResetStates()
+        {
+            JsonStructure jsonStructure = CurrentStructure();
+            return jsonStructure.ResetStates;
+        }
+
+        public void SetResetStates(string[] states)
+        {
+            var structure = CurrentStructure();
+            structure.ResetStates = states;
+            SaveNewStructure(structure);
+        }
+    }
+}
